0488e0a75d8ddc17ef2480ac243a72b1b4ea4946
Happiness Neema Jr. <99131294+appjr5@users.noreply.github.com>
2026-05-29 15:15:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
-from db import models  # noqa: F401
+from db import models
 from db.database import engine, Base
+from routes.chat_routes import router as chat_router
+from routes.pregnancy_routes import router as pregnancy_router
+from routes.auth_routes import router as auth_router
+from routes.ai_routes import router as ai_router
 
 app = FastAPI()
-# print("Hello World")
 
 Base.metadata.create_all(bind=engine)
-print("Database tables created successfully.")
 
 @app.get("/")
 def home():
     return {"message": "DB initialized successfully"}
+
+app.include_router(chat_router)
+app.include_router(pregnancy_router)
+app.include_router(auth_router)
+app.include_router(ai_router)
